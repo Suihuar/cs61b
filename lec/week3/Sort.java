c4a87e50cab7f3a07698768a4919ec1dda3563a2
Suihua <2254579398@qq.com>
2023-06-06 00:31:00 +0800

    add new

---
+++ lec/week3/Sort.java
@@ -0,0 +1,39 @@
+public class Sort{
+//    find the smallest item
+//    move it to the front
+//    selection sort the rest (recursion)
+
+//    return the index of the smallest item in x, starts at the position a
+    public static int findSmallest(String[] x, int start){
+        int smallestIndex = start;
+        for (int i = start; i < x.length; i += 1){
+            //if x[i]<x[smallestIndex], cmp will be -1.
+            int cmp = x[i].compareTo(x[smallestIndex]);
+            if(cmp < 0) {
+                smallestIndex = i;
+            }
+        }
+        return smallestIndex;
+    }
+
+    //swapping the items x[a] and x[b]
+    public static void swap(String[] x, int a, int b){
+        String temp= x[a];
+        x[a] = x[b];
+        x[b] = temp;
+    }
+
+
+    public static void sort(String[] x){
+        sort(x,0);
+    }
+//    recursion helper: sorts the rest of the array that starts at some point
+    private static void sort(String[] x, int start){
+        if(start == x.length) {
+            return;
+        }
+        int smallest = findSmallest(x,start);
+        swap(x, smallest, start);
+        sort(x, start + 1);
+    }
+}
--- lec/week3/TestSort.java
+++ lec/week3/TestSort.java
@@ -1,17 +1,36 @@
 public class TestSort{
     
     /*Sort strings destructively*/
-    public static void testsort(){
-         String[] input = {"I","have", "an","egg"};
-         String[] expected = {"an", "egg", "have", "I"};
+    @org.junit.Test
+    public void testSort(){
+         String[] input = {"i","have", "an","egg"};
+         String[] expected = {"an", "egg", "have", "i"};
          Sort.sort(input);
 
-         if (java.util.Arrays.equals(input,expected)){
-            System.out.println("Error!");
+         org.junit.Assert.assertArrayEquals(expected,input);
          }
+    @org.junit.Test
+    public void testSwap(){
+        String[] input = {"i","have", "an","egg"};
+        int a = 0;
+        int b = 2;
+        String[] expected = {"an","have", "i", "egg"};
+
+        Sort.swap(input,a,b);
+        org.junit.Assert.assertArrayEquals(expected,input);
+    }
+    /*Test the Sort,findSmallest method*/
+    @org.junit.Test
+    public void testFindSmallest() {
+        String[] input = {"i", "have", "an", "egg"};
+        String expected = "an";
+        String actual = input[Sort.findSmallest(input,0)];
+        org.junit.Assert.assertEquals(expected,actual);
+
+        String[] input2 = {"an", "have", "i", "egg"};
+        String expected2 = "egg";
+        String actual2 = input[Sort.findSmallest(input2,2)];
+        org.junit.Assert.assertEquals(expected2,actual2);
     }
 
-    public static void main(String[] args){
-        testsort();
-    }
 }
